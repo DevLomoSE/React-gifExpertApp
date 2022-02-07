40f1420998b52867e528aa339c8f334d37fda1d3
jonathan <jonathancggs@gmail.com>
2022-02-07 17:43:58 -0600

    :recycle: Category
    adding complete functionality between components to add new category to an existing list of it

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -6,18 +6,11 @@ export const GifExpertApp = () => {
 
   const [categories, setCategories] = useState(['One punch', 'Samurai X', 'Yuhi-oh']);
 
-  // const handleAdd = (category) => {
-  //   category = 'cat';
-  //   setCategories( cats => [...cats, category]);
-  // };
-
   return (
   <>
     <h2>GifExpertApp</h2>
-    <AddCategory />
+    <AddCategory setCategories={ setCategories } />
     <hr />
-
-    {/* <button onClick={ handleAdd }>Add category</button> */}
 
     <ol>
       {categories.map( (cat, index) => {
--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-export const AddCategory = () => {
+/**
+ * Component to handle the functionality to add new categories.
+ *
+ * @param setCategories function to mutate the categories state from parent
+ */
+export const AddCategory = ({ setCategories }) => {
 
     const [inputValue, setInputValue] = useState('');
 
@@ -10,17 +16,22 @@ export const AddCategory = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('submited')
+        if(inputValue.trim().length > 0){
+            setCategories((cats) => [...cats, inputValue]);
+            setInputValue("");
+        }
     }
 
   return (
     <>
       <h2>AddCategory</h2>
-      <form onSubmit={ handleSubmit }>
-        <input type="text" 
-            value={inputValue} 
-            onChange={handleInputChange} />
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
       </form>
     </>
   );
 };
+
+AddCategory.propTypes = {
+    setCategories: PropTypes.func.isRequired
+}
