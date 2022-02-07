a60ac6ba1959303d8fc13b190f5a7027f0953ab7
jonathan <jonathancggs@gmail.com>
2022-02-07 13:05:55 -0600

    :recycle: Main app
    update index to render our main component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GifExpertApp from './components/GifExpertApp';
 import './styles/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GifExpertApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
