272dc4a6d98252286b9a916292791247ec586afa
jonathan <jonathancggs@gmail.com>
2022-02-07 14:00:23 -0600

    :construction: handleAdd
    adding the stateHook to create an array fo categories and try to update it

--- src/components/GifExpertApp.js
+++ src/components/GifExpertApp.js
@@ -1,11 +1,27 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One punch', 'Samurai X', 'Yuhi-oh']);
+
+  const handleAdd = (category) => {
+    category = 'skdf';
+    setCategories( cats => [...cats, category]);
+  };
+
   return (
   <>
     <h2>GifExpertApp</h2>
     <hr />
+
+    <button onClick={ handleAdd }>Add category</button>
+
+    <ol>
+      {categories.map( (cat, index) => {
+        return <li key={index+1}>{cat}</li>;
+      } )}
+    </ol>
   </>
   );
 };
