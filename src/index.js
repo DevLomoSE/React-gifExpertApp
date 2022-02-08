cd44705a76fdae8df50baf7f583db9fc28a68544
jonathan <jonathancggs@gmail.com>
2022-02-07 21:35:45 -0600

    :heavy_plus_sign: Animate.css
    adding the dependency for animate.css and setup up it globlaly

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GifExpertApp from './GifExpertApp';
+
 import './styles/index.css';
+import "animate.css";
 
 ReactDOM.render(
   <React.StrictMode>
