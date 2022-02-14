2d97ab5d3f198ff5c2b6524ca3dbb259230c6ed5
jonathan <jonathancggs@gmail.com>
2022-02-14 11:45:33 -0600

    :recycle: PropTypes
    adding the proptypes needed for the GifGridItem

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import { useFetchGifs } from "../hooks/useFetchGifs";
 import GifGridItem from "./GifGridItem";
 
@@ -33,5 +34,9 @@ const GifGrid = ({ category }) => {
     </>
   );
 };
+
+GifGrid.proptype = {
+  category: PropTypes.string.isRequired
+}
 
 export default GifGrid;
